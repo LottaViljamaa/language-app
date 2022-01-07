b4a47bc21ccb0d5789b11a53a523d67976ed47b7
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-07 23:01:57 +0200

    Create structure to admin page.

---
+++ frontend/src/Admin.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function AdminView() {
+    return (
+      <div>
+        <h1>This page is for Admin!</h1>
+      </div>
+    ) 
+}
+
+export default AdminView;
