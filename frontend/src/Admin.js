eab61ecf2e182ce128a63efd317fe81b5dbc5f54
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 11:25:41 +0200

    Add database connection and return data

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,10 +1,35 @@
 import './App.css';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import AdminComponent from './AdminComponent';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+
+
 
 function AdminView() {
+  const [state, setState] = useState([]);
+  const [id, setId] = useState('');
+  const [tagWord, setTagWord] = useState('');
+  const [englisWord, setEnglisWord] = useState('');
+  const [finnishWord, setFinnishWord] = useState('');
+
+  //Connect to database and return all data when changes are made on the page
+  useEffect(() => {
+      fetch("http://localhost:8080/languageApp/")
+        .then(response => response.json())
+        .then(data => setState(data))
+  },[])
+
     return (
-      <div>
+      <div> 
         <h1>This page is for Admin!</h1>
+
+        {/*Go trough the data of database and return all data to the list. */}
+        <ul>
+        {state.map((id, index) => (<AdminComponent key={index} id={id.id} tag={id.tag} english={id.english} finnish={id.finnish} /> ))}
+        </ul>
+        
       </div>
     ) 
 }
