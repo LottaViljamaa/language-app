39e36414bf081bdab21803a27de3b7a1dc37d6fb
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 12:27:56 +0200

    Add rightAnswer -function

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -3,6 +3,12 @@ import { useState } from 'react';
 import TableComponent from './TableComponent';
 import TableComponent2 from './TableComponent2';
 
+  //initialize the variable, which will count the score of right answers. 
+  let score = 0;
+
+  //Initialize the empty list, where right answers will be puttted. 
+  var list = [];
+
 function UserView ({checkInput}) {
   const [state, setState] = useState([]);
   const [state2, setState2] = useState([]);
@@ -14,7 +20,7 @@ function UserView ({checkInput}) {
   //Go trough data from the database and put it to the variable. 
   //Return data and TAblekomponenct, which sohws chosen content.
   const k = componenets.map((index) => {
-    return <TableComponent key={index} english={index.english} finnish={index.finnish}/>;
+    return <TableComponent key={index} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer}/>;
   });
   setState(k);
 }
@@ -27,9 +33,19 @@ function UserView ({checkInput}) {
      //Go trough data from the database and put it to the variable. 
     //Return data and TAblekomponenct, which sohws chosen content.
     const k = componenets2.map((index) => {
-      return <TableComponent2  key={index} english={index.english} finnish={index.finnish} />;
+      return <TableComponent2  key={index} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer}/>;
     });
     setState(k);
+  }
+
+   //This function is called, id user's answer is right
+  //Take right answer and put it to the list.
+  //Grow score 
+  function rightAnswer(a) {
+    score ++;
+    console.log("Tämä on a : " + a);
+    console.log("Your score is: " + score);
+    list.push(a);
   }
   return (
     <div>
