197c097c6873b66360798e2d687332bb5da1f3e0
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 12:03:48 +0200

    Add validation for user input

--- frontend/src/TableComponent.js
+++ frontend/src/TableComponent.js
@@ -5,10 +5,19 @@ function TableComponent(props) {
 
   //Take user input
   function checkInput(e) {
-    const rightAnswer = e.target.value
+    const rightAnswer1 = e.target.value
+
+    //Lowercase the user input
+    const rightAnswer2 = rightAnswer1.toLowerCase();
+
+    //Delete extra spaces from the end of the user input  
+    const removeExtraSpace = (s) => s.trim().split(/ + /).join(" ");
+
+    //Set edited input into the variable
     //If user input matches to the word in the database, 
     //it will put to the list with other right words.
     //Otherwise it will put to the list with other wrong answers. 
+    const rightAnswer = removeExtraSpace(rightAnswer2);
       if (rightAnswer===props.finnish) {
         props.rightAnswer(rightAnswer);
       } else {
