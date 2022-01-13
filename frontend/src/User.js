e020f7a363928d6186518631bb540d952ec89154
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 12:15:38 +0200

    Delete getColors and getAnimals -functions

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -6,8 +6,6 @@ import TableComponent2 from './TableComponent2';
 function UserView ({checkInput}) {
   const [state, setState] = useState([]);
   const [state2, setState2] = useState([]);
-  const [colors, setColors] = useState([]);
-  const [animals, setAnimals] = useState([]);
 
   function getAll() {
     fetch("http://localhost:8080/languageApp/")
@@ -19,18 +17,6 @@ function UserView ({checkInput}) {
     fetch("http://localhost:8080/languageApp/")
       .then(responde => responde.json())
       .then(data => setState2(data))
-  }
-
-  function getColors() {
-    fetch("http://localhost:8080/languageApp/english")
-      .then(responde => responde.json())
-      .then(data => setColors(data))
-  }
-
-  function getAnimals() {
-    fetch("http://localhost:8080/languageApp/animals")
-      .then(responde => responde.json())
-      .then(data => setAnimals(data))
   }
 
   return (
@@ -51,28 +37,11 @@ function UserView ({checkInput}) {
       <ul>
         {state2.map((id) => (<TableComponent2 finnish={id.finnish}/> ))}
       </ul>
-      
-      
-      <button className='getColors'
-        onClick={getColors}>Learn colors
-      </button>
-      
-      <ul>
-        {colors.map((id) => (<TableComponent english={id.english}/> ))}
-      </ul>
 
       <button 
       onClick={() => checkInput()}
         >Submit answers
       </button>
-
-      <button className='getanimals'
-        onClick={getAnimals}>Learn animals
-      </button>
-
-      <ul>
-        {animals.map((id) => (<TableComponent english={id.english}/> ))}
-      </ul>
     </div>
   )
 }
