c7b446c71ba90f55901cdb1f5bec0a39af54a5b1
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 12:35:49 +0200

    Add comment

--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -4,6 +4,7 @@ import React from 'react';
 function Welcome() {
     return (
       <div>
+        {/* Return the content of the first page of the app.*/}
         <h1>Welcome</h1>
       </div>
     ) 
