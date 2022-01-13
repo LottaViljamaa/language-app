2ea983c5622f5a47ce17b231dbf2793a19105a33
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 15:38:49 +0200

    Add material UI

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './App.css';
 import { useState } from 'react';
 import TableComponent from './TableComponent';
 import TableComponent2 from './TableComponent2';
 import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
   //initialize the variable, which will count the score of right answers. 
   let score = 0;
@@ -13,6 +20,7 @@ import Button from '@mui/material/Button';
 function UserView ({checkInput}) {
   const [state, setState] = useState([]);
   const [answerList, setAnswerList] = useState([]);
+  const [component, setComponent] = useState([]);
 
   //Connects to the database
   const getAll = async () => {
@@ -21,9 +29,18 @@ function UserView ({checkInput}) {
     //Go trough data from the database and put it to the variable. 
     //Return data and TAblekomponenct, which sohws chosen content.
     const k = componenets.map((index) => {
-      return <TableComponent key={index} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer} wrongAnswer={wrongAnswer}/>;
+      return <TableComponent key={index} category={index.tag} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer} wrongAnswer={wrongAnswer}/>;
     });
     setState(k);
+    setComponent(
+      <TableHead className="Tableheader">
+      <TableRow>
+        <TableCell>Finnish</TableCell>
+        <TableCell align="right">English</TableCell>
+        <TableCell align="right">Category</TableCell>
+      </TableRow>
+    </TableHead>
+    );
   }
 
   //Connects to the database
@@ -34,9 +51,18 @@ function UserView ({checkInput}) {
     //Go trough data from the database and put it to the variable. 
     //Return data and TAblekomponenct, which sohws chosen content.
     const k = componenets2.map((index) => {
-      return <TableComponent2  key={index} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer} wrongAnswer={wrongAnswer}/>;
+      return <TableComponent2  key={index} category={index.tag} english={index.english} finnish={index.finnish} rightAnswer={rightAnswer} wrongAnswer={wrongAnswer}/>;
     });
     setState(k);
+    setComponent(
+      <TableHead>
+      <TableRow>
+        <TableCell>English</TableCell>
+        <TableCell align="right">Finnish</TableCell>
+        <TableCell align="right">Category</TableCell>
+      </TableRow>
+    </TableHead>
+    );
   }
 
   //This function is called, id user's answer is right
@@ -63,16 +89,23 @@ function UserView ({checkInput}) {
   return (
     <div>
       {/* Return buttons. User can decide the learning language. */}
-      <button className='getAll'
-        onClick={getAll}>Learn in Finnish
-      </button>
-
-      <button className='getAll2'
+      <Button className='getAll'
+        onClick={getAll}>Learn in Finnish 
+        
+      </Button>
+    
+      <Button className='getAll2'
         onClick={getAll2}>Learn in English
-      </button>
-      <ul>
-        {state}
-      </ul>
+      </Button>
+
+      <TableContainer>
+        <Table sx={{maxWidth: 400}}>
+          {component}
+          <TableBody>
+            {state}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
        {/* Return submit answer -button. It will call the useAnswer -function. */}
        <Button 
