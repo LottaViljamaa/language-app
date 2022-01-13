33238bbcc34c004139662e79d2ed938f5b26f431
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-01-13 12:01:03 +0200

    Edit cechInput -function to take and check user input

--- frontend/src/TableComponent2.js
+++ frontend/src/TableComponent2.js
@@ -1,31 +1,35 @@
 import React from "react";
-// import { useState } from "react";
-import UserView from "./User";
 
 
 function TableComponent2(props) {
-  // const [check, setCheck] = useState([]);
+ 
+  function checkInput(e) {
+     //Take user input
+    const rightAnswer = e.target.value
 
-  //Tee virheentarkistus tähän!
-
-   const checkInput = () => {
-    if (props.finnish==="kissa") {
-      console.log("JEE");
-    }
+    //If user input matches to the word in the database, 
+    //it will put to the list with other right words.
+    //Otherwise it will put to the list with other wrong answers. 
+      if (rightAnswer===props.english) {
+        props.rightAnswer(rightAnswer);
+      } else {
+        props.wrongAnswer(rightAnswer);
+      }
   }
+
 return ( 
   <div>
-    <ul>
-      
+    {/* Return  input field and Finnish word. OnBlur calls the checkInput function, which checks user's input. */}
+    <ul key={props.id}> 
       <input
-      placeholder="Make your quest"
-      onClick={checkInput}
-      ></input>{props.finnish}
+      placeholder="Write your answer"
+      onBlur={checkInput}
+      ></input>{ " " + props.finnish}
     </ul>
-    <button></button>
   </div>
 )
 }
+
 
 export default TableComponent2;
 
